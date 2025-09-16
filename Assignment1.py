6b760b475d7a59e8041a0e82e6cc09c55ce08b3d
MalikAlmasri <malikalmasri2000@gmail.com>
2025-09-16 12:48:38 -0400

    first 4 functions complete

--- Assignment1.py
+++ Assignment1.py
@@ -1,32 +1,39 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    maxnum = max(num1, num2, num3)
+    return maxnum
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    minnum = min(num1, num2, num3)
+    return minnum
 
 # Function 3: Conditional Statements – The If Statement
 # This function should check if a number is positive, negative, or zero and return the corresponding message.
 def check_number(number):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    if number == 0:
+        number = "Zero"
+    elif number > 0:
+        number = "Positive"
+    elif number < 0:
+        number = "Negative"
+    return number
 
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
-def star_shape(rows):
-    # TODO: Implement this function
-    pass  # Replace with your code
+def star_shape(rows): 
+    star = ""
+    for x in range(rows): 
+        star += ("*" * (x+1) + "\n")
+    return star.rstrip()
+ 
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
 def count_multiples_of_3(limit):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    
 
 # Function 6: Sum of Even Numbers in a Range
 # This function should calculate and return the sum of even numbers within a given range.
